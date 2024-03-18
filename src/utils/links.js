e47b6c8c5b0cc26485b2ff1d5444cdae3654c7dc
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-03-18 12:44:14 +0300

    Revert feat: cache image

--- src/utils/links.js
+++ src/utils/links.js
@@ -4,7 +4,7 @@ export const bookLink = (title) => `/book-notes/${kebabCase(title)}/`
 export const bookImgLink = (title) => `/images/books/${kebabCase(title)}.jpg`
 
 export const projectImgLink = (title) =>
-  `/images/projects/${kebabCase(title)}?ts=1.jpg`
+  `/images/projects/${kebabCase(title)}.jpg`
 
 export const handleNavPage = (title) => `/${kebabCase(title)}`
 export const handleNavSection = (title) => `/#${kebabCase(title)}`
