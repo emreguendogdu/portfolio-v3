9a92d7c400e761ad8a1868fdacb8ead460244e78
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-03-18 15:39:27 +0300

    chore: Edit project content messages

--- content/projectContents.js
+++ content/projectContents.js
@@ -1,7 +1,7 @@
 export const projectContents = [
   {
     title: "Fitness Website",
-    body: "With using React, created reusable components that can further be used to improve this 111 Fitness Page or to create new fitness websites. Used styled-components to style the components and framer-motion to add animations. Works without an error on mobile devices.",
+    body: "By using React, created reusable components that can further be used to improve this 111 Fitness Page or to create new fitness websites. Used styled-components to style the components and framer-motion to add animations. Implemeted a blur loading system with small versions for images. Works flawlessly on mobile devices.",
     tags: ["React", "styled-components", "framer-motion"],
   },
   {
@@ -13,11 +13,11 @@ export const projectContents = [
   {
     title: "Data Visualization Projects",
     body: "Worked with five different datasets and used D3.js to display them. Created a bar chart, scatter plot, heat map, choropleth map, and a treemap. With the use of React, merged all projects into one app. On treemap, handled three different datasets on single page.",
-    tags: ["HTML", "CSS", "React", "D3", "APIs"],
+    tags: ["HTML", "CSS", "React", "D3.js", "APIs"],
   },
   {
     title: "Pomodoro Clock",
-    body: "While creating this pomodoro timer project, I used React. By using it, it was easier to work with the state management, timer logic and dividing the app to eliminate the confusion. Also, there are two themes to choose from: space and nature. More, the app offers four different work - break timer presets, and it rings when the timer ends. Responsively designed.",
+    body: "Used React, its state management and component management to create this project. Two themes presented to the user: Forest and Space. More, the app offers four different work - break timer presets, ring when the timer goes off and website title change each second so that you can see how much time left while surfing on another tab. Works on your mobile devices.",
     tags: ["HTML", "CSS", "React"],
   },
   {
@@ -27,7 +27,7 @@ export const projectContents = [
   },
   {
     title: "Random Quote Generator",
-    body: "Worked with Random Quote Generator API to fetch quotes and display them on the page. Used React to create the app, and react-spinners to create a loading spinner. App is fully responsive and changes color with each new quote.",
+    body: "Worked with Random Quote Generator API to fetch quotes and display them on the page. Used React to create the app, and react-spinners to create a loading spinner. App is fully responsive and theme color changes with each new quote.",
     tags: ["HTML", "CSS", "APIs", "React"],
     color: "#36191d",
   },
