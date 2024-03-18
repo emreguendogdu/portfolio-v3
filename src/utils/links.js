1527c7f4f1e89c45915916ecc4d85de90131fc7d
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-03-18 12:42:50 +0300

    feat: cache image

--- src/utils/links.js
+++ src/utils/links.js
@@ -4,8 +4,7 @@ export const bookLink = (title) => `/book-notes/${kebabCase(title)}/`
 export const bookImgLink = (title) => `/images/books/${kebabCase(title)}.jpg`
 
 export const projectImgLink = (title) =>
-  `/images/projects/${kebabCase(title)}.jpg`
-
+  `/images/projects/${kebabCase(title)}?ts=1.jpg`
 
 export const handleNavPage = (title) => `/${kebabCase(title)}`
 export const handleNavSection = (title) => `/#${kebabCase(title)}`
