737b8118dccd5d53fd3e6fcd236bcb55ed7516c9
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-05-06 13:13:41 +0300

    fix: add page.js to app (cp from [[...slug]])

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import "@/globals.css"
+import App from "@/App"
+
+export const metadata = {
+  title: "Osman Emre Gundogdu",
+  description: "Osman Emre Gundogdu | Personal website, portfolio",
+  keywords:
+    "Osman Emre Gundogdu, Osman, Emre, Gundogdu, Gündoğdu, Frontend, Developer, Web, Portfolio, Personal, Website, Book, Notes, Software, Engineer, Front, End, Full, Fullstack, Stack, osmangund, Canother, wyarn, osmangund.github.io",
+}
+
+export default function MyApp() {
+  return (
+    <>
+      <main>
+        <App />
+      </main>
+    </>
+  )
+}
