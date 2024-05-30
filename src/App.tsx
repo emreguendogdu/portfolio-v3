1ea548671ef3b279e47436b82d80ac0eb18fa942
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-05-30 13:04:21 +0300

    fix: nav not appearing

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Contact from "./components/sections/contact/contact"
 import About from "./components/about/about"
 import Projects from "./components/sections/Projects/projects"
 import Hero from "./components/sections/Header/hero"
+import Nav from "./components/Nav/nav"
 
 export default function App() {
   return (
     <>
       <LazyMotion features={domAnimation}>
+        <Nav />
         <Hero />
         <About />
         <Projects />
