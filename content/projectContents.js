a1941ac308e5f5463925152276bceb5bf403e5c7
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-09-19 20:24:05 +0300

    chore: change project order

--- content/projectContents.js
+++ content/projectContents.js
@@ -1,21 +1,8 @@
 export const projectContents = [
   {
-    title: "Laodikya Web",
-    body: "",
-    tags: ["HTML", "SCSS", "Astro"],
-    href: "https://laodikyaweb.com",
-  },
-  {
-    title: "Onur Arac Tattoo Art",
-    body: "",
-    tags: ["HTML", "SCSS", "Astro"],
-    href: "https://onurarac.com",
-  },
-  {
-    title: "111 Fitness",
-    body: "By using React, created reusable components that can further be used to improve this 111 Fitness Page or to create new fitness websites. Used styled-components to style the components and framer-motion to add animations. Implemeted a blur loading system with small versions for images. Works flawlessly on mobile devices.",
-    tags: ["React", "framer-motion", "styled-components"],
-    href: "https://osmangund.github.io/111-fitness",
+    title: "Zen Tattoo",
+    tags: ["React", "Next.js", "framer-motion"],
+    href: "https://zen-tattoo.vercel.app/",
   },
   {
     title: "Letters App",
@@ -25,9 +12,10 @@ export const projectContents = [
     href: "https://letters-app.vercel.app/",
   },
   {
-    title: "Zen Tattoo",
-    tags: ["React", "Next.js", "framer-motion"],
-    href: "https://zen-tattoo.vercel.app/",
+    title: "111 Fitness",
+    body: "By using React, created reusable components that can further be used to improve this 111 Fitness Page or to create new fitness websites. Used styled-components to style the components and framer-motion to add animations. Implemeted a blur loading system with small versions for images. Works flawlessly on mobile devices.",
+    tags: ["React", "framer-motion", "styled-components"],
+    href: "https://osmangund.github.io/111-fitness",
   },
   {
     title: "Data Visualization Projects",
@@ -36,4 +24,16 @@ export const projectContents = [
     tags: ["React", "D3.js", "HTML", "CSS", "APIs"],
     href: "https://osmangund.github.io/data-visualization-projects",
   },
-];
+  {
+    title: "Onur Arac Tattoo Art",
+    body: "",
+    tags: ["HTML", "SCSS", "Astro"],
+    href: "https://onurarac.com",
+  },
+  {
+    title: "Laodikya Web",
+    body: "",
+    tags: ["HTML", "SCSS", "Astro"],
+    href: "https://laodikyaweb.com",
+  },
+]
